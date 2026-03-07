03f3733752ff17387a0839a87a1b7d1a90f31448
Yatharth Shahi <boleto.imspooky@gmail.com>
2026-03-07 16:28:34 +0530

    Added SharedState variables for Fan Control

--- src/main/java/daemon/DaemonState.java
+++ src/main/java/daemon/DaemonState.java
@@ -6,6 +6,19 @@ public class DaemonState {
         STATIC, RAINBOW, OFF
     }
 
+    public enum FANMode {
+        MAX, MANUAL, AUTO
+    }
+
+    public volatile FANMode fanMode = FANMode.AUTO;
+    public volatile int fan1_max;
+    public volatile int fan1_target;
+    public volatile int fan1_input;
+    public volatile int fan2_max;
+    public volatile int fan2_target;
+    public volatile int fan2_input;
+
+
     public volatile RGBMode rgbMode = RGBMode.RAINBOW;
     public volatile int r = 255;
     public volatile int g = 0;
