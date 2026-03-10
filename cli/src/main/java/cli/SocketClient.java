295fe2d3c564b5efd4361a1dfb460e32b14c9fd9
Yatharth Shahi <boleto.imspooky@gmail.com>
2026-03-10 21:02:23 +0530

    Implemented Unix Domain socket communication between CLI and Daemon via SocketClient.java

--- cli/src/main/java/cli/SocketClient.java
+++ cli/src/main/java/cli/SocketClient.java
@@ -1,8 +1,33 @@
 package cli;
 
-public class SocketClient implements Runnable{
-    @Override
-    public void run() {
+import core.DaemonState;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.StandardProtocolFamily;
+import java.net.UnixDomainSocketAddress;
+import java.nio.channels.Channels;
+import java.nio.channels.SocketChannel;
+import java.nio.file.Path;
+
+public class SocketClient{
+
+
+    public SocketClient(){
+
+    }
+
+    public void sendCommand(String command) throws IOException {
+        Path socketPath = Path.of("/run/victus-control/victus.sock");
+        UnixDomainSocketAddress socketAddress = UnixDomainSocketAddress.of(socketPath);
+
+        SocketChannel channel = SocketChannel.open(StandardProtocolFamily.UNIX);
+        channel.connect(socketAddress);
+        System.out.println("Socket connected at /run/victus-control/victus.sock");
+        PrintWriter out = new PrintWriter(Channels.newOutputStream(channel), true);
+        out.println(command);
+        channel.shutdownOutput();
+
 
     }
 }
