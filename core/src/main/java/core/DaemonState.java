3df63ad30270f70306287635061e593871120371
Yatharth Shahi <boleto.imspooky@gmail.com>
2026-03-13 17:27:55 +0530

    Made sharedState fields static

--- core/src/main/java/core/DaemonState.java
+++ core/src/main/java/core/DaemonState.java
@@ -4,23 +4,23 @@ import java.nio.file.Path;
 
 public class DaemonState {
 
-    public volatile Path hwmonPath;
+    public static volatile Path hwmonPath;
 
-    public volatile FANMode fanMode = FANMode.AUTO;
-    public volatile int fan1_max;
-    public volatile int fan1_target = 2500;
-    public volatile int fan1_input;
-    public volatile int fan2_max;
-    public volatile int fan2_target = 2500;
-    public volatile int fan2_input;
-    public volatile int pwmMode;
+    public static volatile FANMode fanMode = FANMode.AUTO;
+    public static volatile int fan1_max;
+    public static volatile int fan1_target = 2500;
+    public static volatile int fan1_input;
+    public static volatile int fan2_max;
+    public static volatile int fan2_target = 2500;
+    public static volatile int fan2_input;
+    public static volatile int pwmMode;
 
-    public volatile RGBMode rgbMode = RGBMode.RAINBOW;
-    public volatile int r = 255;
-    public volatile int g = 0;
-    public volatile int b = 0;
-    public volatile int rgbSpeed = 20;
-    public volatile int brightness = 255;
+    public static volatile RGBMode rgbMode = RGBMode.RAINBOW;
+    public static volatile int r = 255;
+    public static volatile int g = 0;
+    public static volatile int b = 0;
+    public static volatile int rgbSpeed = 20;
+    public static volatile int brightness = 255;
 
 
 }
